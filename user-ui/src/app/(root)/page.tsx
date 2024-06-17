b7894deddd5ca86074711e68de06e975c88ee92b
razafindratsiza <ainarazafindratsiza23@gmail.com>
2024-06-17 21:09:56 +0300

    setup next js app

---
+++ user-ui/src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+
+import HomeScreen from '@/screens/HomeScreen'
+import React from 'react'
+
+const  Page = ()=> {
+  return (
+    <HomeScreen/>
+  )
+}
+
+export default Page
---
+++ user-ui/src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Layout/Header'
+
+const  HomeScreen = ()=> {
+  return (
+   <div>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
